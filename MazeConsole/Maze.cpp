af6599c0cbe0db95b56f215b39018cf41c94f729
danielc6754 <choimi@oregonstate.edu>
2019-05-09 03:15:34 -0700

    Add project files.

---
+++ MazeConsole/Maze.cpp
@@ -0,0 +1,29 @@
+using namespace std;
+
+#include "ConsoleEngine.h"
+
+class Console_Maze : public ConsoleTemplateEngine {
+public:
+	Console_Maze() {
+		//m_sAppName = L"Maze";
+	}
+
+protected:
+	virtual bool OnUserCreate() {
+		return true;
+	}
+
+	virtual bool OnUserUpdate(float fElapsedTime) {
+		return true;
+	}
+};
+
+int main() {
+	// Use ConsoleEngine derived app
+	Console_Maze game;
+	game.ConstructConsole(80, 50, 8, 8);
+	game.Start();
+	
+	
+	return 0;
+}
